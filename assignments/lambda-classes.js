2cb8553be60e46d1b3597e792419359d2faaad81
Poly Goveia <poly.goveia@gmail.com>
2019-01-25 18:53:39 -0500

    add Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,18 @@ class Person {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
     }
 }
+
+class Instructor extends Person {
+    constructor(attributes){
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student) {
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    }
+}
